d1b400eff5cd3e87b045416fe6fd98e98d79690e
0chotona <eunjongh97@gmail.com>
2026-06-08 12:22:53 +0900

    [KAN-1201] chore: 테스트 전용 secret 추가

--- src/test/java/com/erumpay/api_gateway/ApiGatewayApplicationTests.java
+++ src/test/java/com/erumpay/api_gateway/ApiGatewayApplicationTests.java
@@ -3,7 +3,10 @@ package com.erumpay.api_gateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+	"jwt.secret=test-jwt-secret-key-must-be-at-least-256-bits-long-for-hs256",
+	"pg.jwt.secret=test-pg-jwt-secret-key-must-be-at-least-256-bits-long-for-hs256"
+})
 class ApiGatewayApplicationTests {
 
 	@Test
